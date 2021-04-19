41e58f28fc2b45904ee2f7715ccb5cfe565fd7a1
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-19 17:38:00 +0200

    ajout video tips audio effect OK

--- controllers/tipsController.js
+++ controllers/tipsController.js
@@ -1,4 +1,3 @@
-// const tipsDataMapper = require('../dataMappers/tipsDataMapper'); 
 const Vimeo = require('vimeo').Vimeo;
 const client = new Vimeo(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.ACCESS_TOKEN);
 
@@ -8,6 +7,36 @@ module.exports = {
             client.request({
                 method: 'GET',
                 path: '/tags/Audio_effect/videos'
+              }, function (error, body, status_code, headers) {
+                res.json({ ...[body.data] });
+              
+                console.log(body, status_code);
+              })
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    getMidiEffect: async function(req, res, next) {
+        try { 
+            client.request({
+                method: 'GET',
+                path: '/tags/Midi_effect/videos'
+              }, function (error, body, status_code, headers) {
+                res.json({ ...[body.data] });
+              
+                console.log(body, status_code);
+              })
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    getMidiInstrument: async function(req, res, next) {
+        try { 
+            client.request({
+                method: 'GET',
+                path: '/tags/Midi_instrument/videos'
               }, function (error, body, status_code, headers) {
                 res.json({ ...[body.data] });
               
--- routers/tipsRouter.js
+++ routers/tipsRouter.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const tipsController = require('../controllers/tipsController');
 
 router.get('/audio-effect', tipsController.getAudioEffect);
+router.get('/midi-effect', tipsController.getMidiEffect);
+router.get('/midi-instrument', tipsController.getMidiInstrument);
 
 module.exports = router;
