9195a15e61b68e94ea179862f00c716f690cf677
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-20 18:12:42 +0200

    test jwt 2

--- routers/authentificationMiddleware.js
+++ routers/authentificationMiddleware.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express(); 
+
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+   next();
+ };
+  app.use(allowCrossDomain);
+
+
 module.exports = {
     /* Récupération du header bearer */
      extractBearerToken: async function (headerValue) {
