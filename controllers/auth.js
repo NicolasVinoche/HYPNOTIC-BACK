2735e855a45052d283d1ed96e63f2eaca65671ed
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-23 15:00:57 +0200

    test auth 2

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,8 +3,9 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
       // On récupère le token dans le header Authorization de la requête, on split pour récuperer tout après l'espace
-      const token = req.headers.token.replace('token=', '');
-      console.log('token:', token)
+     console.log('AVANT TOKEN')
+      const token = req.headers.cookie.replace('token=', "");
+      console.log('TOKEN!!!!!!!!!!:', token)
       // On décode let token 
       const decodedToken = jwt.verify(token, process.env.JWT_SIGN_SECRET);
       // On extrait l'ID utilisateur dans le token 
