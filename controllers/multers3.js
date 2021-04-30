9b4cc6e27394fef66b2002cb2a327acbf23de910
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-30 12:30:01 +0200

    test multer 2

--- controllers/multers3.js
+++ controllers/multers3.js
@@ -11,7 +11,7 @@ module.exports = {
         description = req.body.description;
         price = req.body.price;
         tag = req.body.tag;
-        file = req.files.file;  
+        file = req.body.file;  
         
         console.log(file);
 
