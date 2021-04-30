bf8d47553c5be2ef3f512652395ee5f4580e359e
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-30 15:43:23 +0200

    test 9

--- controllers/multers3.js
+++ controllers/multers3.js
@@ -4,71 +4,106 @@ const multerS3 = require('multer-s3');
 const { uuid } = require('uuidv4'); 
 const path = require('path');  
 
+
+var creds = new AWS.Credentials({
+    accessKeyId: 'AKIAYDMZRXLLOEPMOD7J', secretAccessKey: '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'
+    });
+
+    // Create S3 service object
+const s3 = new AWS.S3({apiVersion: '2006-03-01', 
+                    credentials: creds}); 
+
+const upload = multer({
+    storage: multerS3({
+        s3, 
+        bucket: 'hypnotic-peafowl', 
+        acl: 'public-read-write',
+        metadata: (req, file, cb) => { 
+            cb(null, { fieldName: file.fieldname});  
+            console.log(file);   
+        }, 
+        key: (req, file, cb) => {
+            const ext = path.extname(file.originalname);
+            cb(null, `${uuid()}${ext}`); 
+            console.log(file);
+        }
+    }) 
+});
+
 module.exports = { 
 
-  newPack: async function(req, res, next) {
+  newPack: upload.single('file'), async function (req, res, next) {
+    file = req.file;
+    console.log(file)
+  }
+  
+  
+//   async function(req, res, next) {
         // title = req.body.title;
         // description = req.body.description;
         // price = req.body.price;
         // tag = req.body.tag;
-        file = req.formData.file;
+        // file = req.file;
         // console.log('REQ :', req);
         //console.log('REQ.BODY :', req.body);
        // console.log('REQ.FILE :', req.file);
 
 
-       console.log('REQ FILE :', file);
-       try {
-        console.log('JE PASSE ICI')
+    //    console.log('REQ FILE :', file);
+    //    try {
+    //     console.log('JE PASSE ICI')
 
-        var creds = new AWS.Credentials({
-            accessKeyId: 'AKIAYDMZRXLLOEPMOD7J', secretAccessKey: '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'
-            });
+        // var creds = new AWS.Credentials({
+        //     accessKeyId: 'AKIAYDMZRXLLOEPMOD7J', secretAccessKey: '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'
+        //     });
         
-            // Create S3 service object
-        const s3 = new AWS.S3({apiVersion: '2006-03-01', 
-                            credentials: creds}); 
+        //     // Create S3 service object
+        // const s3 = new AWS.S3({apiVersion: '2006-03-01', 
+        //                     credentials: creds}); 
         
-        const upload = multer({
-            storage: multerS3({
-                s3, 
-                bucket: 'hypnotic-peafowl', 
-                acl: 'public-read-write',
-                metadata: (req, file, cb) => { 
-                    cb(null, { fieldName: file.fieldname});  
-                    console.log(file);   
-                }, 
-                key: (req, file, cb) => {
-                    const ext = path.extname(file.originalname);
-                    cb(null, `${uuid()}${ext}`); 
-                    console.log(file);
-                }
-            }) 
-        }); 
+        // const upload = multer({
+        //     storage: multerS3({
+        //         s3, 
+        //         bucket: 'hypnotic-peafowl', 
+        //         acl: 'public-read-write',
+        //         metadata: (req, file, cb) => { 
+        //             cb(null, { fieldName: file.fieldname});  
+        //             console.log(file);   
+        //         }, 
+        //         key: (req, file, cb) => {
+        //             const ext = path.extname(file.originalname);
+        //             cb(null, `${uuid()}${ext}`); 
+        //             console.log(file);
+        //         }
+        //     }) 
+        // }); 
 
-        console.log ('JAI FAIS CA');
+        // console.log ('JAI FAIS CA');
         
-        upload.single('file'), async function (err, data, req) {
+        // upload.single('file'), async function (err, data, req) { 
 
-            if (err) {
-                console.log("Error", err);
-            } else {
-                console.log('uploaded:', req.files.length);
+        //     file = req.file; 
+        
 
-                // const newpack = await packDataMapper.insertPack(title, description, price, tag, data.Location);
-                // console.log(newpack);
-                // return res.status(200).json ({newpack}); 
-            }
-        }
+        //     if (err) {
+        //         console.log("Error", err);
+        //     } else {
+        //         console.log('uploaded:', req.files.length);
+
+        //         // const newpack = await packDataMapper.insertPack(title, description, price, tag, data.Location);
+        //         // console.log(newpack);
+        //         // return res.status(200).json ({newpack}); 
+        //     }
+        // }
         
         
-        } catch (error) {
-            next(error)
-            console.log(error)
-        }
+        // } catch (error) {
+        //     next(error)
+        //     console.log(error)
+        // }
 
     
-    }
+    // }
     
 }
 
