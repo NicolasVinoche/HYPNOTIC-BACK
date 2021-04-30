02f976e658d1d1cfc5247590196dfdfb3d417875
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-30 19:01:07 +0200

    req.file

--- routers/settingRouter.js
+++ routers/settingRouter.js
@@ -6,7 +6,7 @@ const multerS3 = require('../controllers/multers3');
 
 router.put('/user/:id', settingController.updateUser);
 router.post('/admin/pack', multerS3.newPack, async (res, req, next) => {
-    console.log('req.file:', req)
+    console.log('req.file:', req.file)
         // title = req.body.title;
         // description = req.body.description;
         // price = req.body.price;
