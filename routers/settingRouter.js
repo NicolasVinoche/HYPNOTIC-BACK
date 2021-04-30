c5baf6e0fbcd830b3b5c9f8cf5f9fa396b1df51f
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-30 18:28:12 +0200

    validé7

--- routers/settingRouter.js
+++ routers/settingRouter.js
@@ -6,7 +6,7 @@ const multerS3 = require('../controllers/multers3');
 
 router.put('/user/:id', settingController.updateUser);
 router.post('/admin/pack', multerS3.newPack, (req, res, next) => {
-console.log('req:', req.file)
+console.log('req:', req.file.location)
 next()
 } ); //settingController.newPack
 router.post('/admin/track', settingController.newTrack);
