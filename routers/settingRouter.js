2d0b454e1b9487ea38698f57eb8349275289cd38
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-04-30 19:09:42 +0200

    req.file4

--- routers/settingRouter.js
+++ routers/settingRouter.js
@@ -5,7 +5,7 @@ const settingController = require('../controllers/settingController');
 const multerS3 = require('../controllers/multers3');
 
 router.put('/user/:id', settingController.updateUser);
-router.post('/admin/pack', multerS3.newPack, async (res, req, next) => {
+router.post('/admin/pack', multerS3.newPack,  (res, req, next) => {
     console.log('req.file:', req.file)
     // console.log('req.title:', req.title)
     // console.log('req.description:', req.description)
@@ -16,13 +16,13 @@ router.post('/admin/pack', multerS3.newPack, async (res, req, next) => {
         // price = req.body.price;
         // tag = req.body.tag;
         // file = req.file.location;
-    try {
-            // const newpack = await packDataMapper.insertPack(title, description, price, tag,req.file.location);
-            //     console.log(newpack);
-            // return res.status(200).json ({newpack}); 
-        } catch (error) {
-            next(error)
-        }
+    // try {
+    //         // const newpack = await packDataMapper.insertPack(title, description, price, tag,req.file.location);
+    //         //     console.log(newpack);
+    //         // return res.status(200).json ({newpack}); 
+    //     } catch (error) {
+    //         next(error)
+    //     }
 } ); //settingController.newPack
 router.post('/admin/track', settingController.newTrack);
 
