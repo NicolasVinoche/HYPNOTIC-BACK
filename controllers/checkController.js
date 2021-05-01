07f2ed30538a1f6778cec0742a6928d67a4f596d
SamiFekkar <mr.fekkar.sami@gmail.com>
2021-05-01 16:40:42 +0200

    modif check

--- controllers/checkController.js
+++ controllers/checkController.js
@@ -1,30 +1,32 @@
-const jwt = require('jsonwebtoken'); 
+const jwt = require('jsonwebtoken');
+const userDataMapper = require('../dataMappers/userDataMapper');
 
 module.exports = {
-    
-    check: async function(req, res, next) {
-        try {
-            // On récupère le token (cookie-parser dans le app)
-            const token = req.cookies.token;
-            console.log('COOKIE:', req.cookies.token);
-            // On décode let token
-            const decodedToken = jwt.verify(token, process.env.JWT_SIGN_SECRET);
-            console.log('decodedToken:', decodedToken)
-            // On extrait l'ID utilisateur dans le token
-            const userId = decodedToken.userId;
-            // On vérifie si ID utilisateur de la demande et on le compare à celui du token
-            console.log('userId :', userId)
-            if (userId) {
-                res.json({decodedToken})
-            }
-            if (error) {
-                next(error);
-            }
-        }   catch {
-            res.status(401).json({
-              error: new Error('Invalid request!'),
-            });
-        }
+  check: async function (req, res, next) {
+    try {
+      // On récupère le token (cookie-parser dans le app)
+      const token = req.cookies.token;
+      console.log('COOKIE:', req.cookies.token);
+      // On décode let token
+      const decodedToken = jwt.verify(token, process.env.JWT_SIGN_SECRET);
+      console.log('decodedToken:', decodedToken);
+      // On extrait l'ID utilisateur dans le token
+      const userId = decodedToken.userId;
+      // On récupere l'user dans la db pour renvoyer les infos
+      const userRefresh = await userDataMapper.findUser(decodedToken.email);
+      console.log('userRefresh:', userRefresh);
+      // On vérifie si ID utilisateur de la demande et on le compare à celui du token
+      console.log('userId :', userId);
+      if (userId) {
+        res.json({ decodedToken });
+      }
+      if (error) {
+        next(error);
+      }
+    } catch {
+      res.status(401).json({
+        error: new Error('Invalid request!'),
+      });
     }
-
-}
+  },
+};
