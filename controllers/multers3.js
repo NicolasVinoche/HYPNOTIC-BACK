84175c7cea59a01255de7a989b0b7c0d8cac92a6
Nicolas Vinoche <nico.vinoche@gmail.com>
2021-05-02 14:16:30 +0200

    status 200

--- controllers/multers3.js
+++ controllers/multers3.js
@@ -46,105 +46,10 @@ const upload = multer({
     }) 
  })
 
-  
-// upload.single('file'), function (req, res, next) {
-          
-//  title = req.body.title;
-//  description = req.body.description;
-//  price = req.body.price;
-//  tag = req.body.tag;
-//  file = req.file;
-//  console.log(title)
-//  console.log('URL :', file)
-
 module.exports = { 
     
-    newPack: upload.single('file'),
+    newPack: upload.single('file'), 
+
+    newTrack: upload.single('file')
     
 }
-    // , function(req, res, next) {
-          
-        // addPack: async function (location) {
-        
-//     //     try {
-//     //         const newpack = await packDataMapper.insertPack(title, description, price, tag, data.Location);
-//     //             console.log(newpack);
-//     //         return res.status(200).json ({newpack}); 
-//     //     } catch (error) {
-//     //         next(error)
-//     //     }
-//     // }
-// }
-
-
-    
-
-
-
-        // title = req.body.title;
-        // description = req.body.description;
-        // price = req.body.price;
-        // tag = req.body.tag;
-    //     file = req.file;
-    //     console.log('REQ :', req);
-    //     console.log('REQ.BODY :', req.body);
-    //    console.log('REQ.FILE :', req.file);
-
-
-    //    console.log('REQ FILE :', file);
-    //    try {
-    //     console.log('JE PASSE ICI')
-
-    //     var creds = new AWS.Credentials({
-    //         accessKeyId: 'AKIAYDMZRXLLOEPMOD7J', secretAccessKey: '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'
-    //         });
-        
-    //         // Create S3 service object
-    //     const s3 = new AWS.S3({apiVersion: '2006-03-01', 
-    //                         credentials: creds}); 
-        
-    //     const upload = multer({
-    //         storage: multerS3({
-    //             s3, 
-    //             bucket: 'hypnotic-peafowl', 
-    //             acl: 'public-read-write',
-    //             metadata: (req, file, cb) => { 
-    //                 cb(null, { fieldName: file.fieldname});  
-    //                 console.log(file);   
-    //             }, 
-    //             key: (req, file, cb) => {
-    //                 const ext = path.extname(file.originalname);
-    //                 cb(null, `${uuid()}${ext}`); 
-    //                 console.log(file);
-    //             }
-    //         }) 
-    //     }); 
-
-    //     console.log ('JAI FAIS CA');
-        
-    //     upload.single('file'), (req) => { 
-
-    //         file = req.file; 
-    //         console.log('uploaded');
-        
-
-    //         //if (err) {
-    //           //  console.log("Error", err);
-    //         //} else {
-
-    //             // const newpack = await packDataMapper.insertPack(title, description, price, tag, data.Location);
-    //             // console.log(newpack);
-    //             // return res.status(200).json ({newpack}); 
-    //        // }
-    //     }
-        
-        
-    //     } catch (error) {
-    //         next(error)
-    //         console.log(error)
-    //     }
-
-    
-    
-
-
--- routers/settingRouter.js
+++ routers/settingRouter.js
@@ -3,14 +3,18 @@ const router = express.Router();
 
 const settingController = require('../controllers/settingController'); 
 const multerS3 = require('../controllers/multers3'); 
-const packDataMapper = require ('../dataMappers/packDataMapper');
+const packDataMapper = require ('../dataMappers/packDataMapper'); 
+const trackDataMapper = require('../dataMappers/tracksDataMapper');
 
 router.put('/user/:id', settingController.updateUser);
+
+// NEW PACK
+
 router.post('/admin/pack', express.json({ limit: '100mb' }) ,multerS3.newPack, async(req, res, next) => {
     console.log('req.file:', req.file)
     console.log('req.title:', req.body.title)
     next()
-    title = req.body.title;
+        title = req.body.title;
         description = req.body.description;
         price = req.body.price;
         tag = req.body.tag;
@@ -18,11 +22,31 @@ router.post('/admin/pack', express.json({ limit: '100mb' }) ,multerS3.newPack, a
     try {
             const newpack = await packDataMapper.insertPack(title, description, price, tag, file);
                 console.log(newpack);
-            // return res.status(200).json ({newpack}); 
+            return res.status(200, 'INSERTION EN BASE DU PACK OK'); 
         } catch (error) {
             next(error)
         }
-} ); //settingController.newPack
-router.post('/admin/track', settingController.newTrack);
+} ); 
+
+// NEW TRACK
+
+router.post('/admin/track', express.json({ limit: '100mb' }) ,multerS3.newTrack, async(req, res, next) => {
+    console.log('req.file:', req.file)
+    console.log('req.title:', req.body.title)
+    next()
+        title = req.body.title; 
+        track_number = req.body.track_number;
+        description = req.body.description;
+        price = req.body.price;
+        album_name = req.body.album_name;
+        file = req.file.location;
+    try {
+            const newtrack = await trackDataMapper.insertTracks(track_number, title, description, price, album_name, file);
+                console.log(newtrack);
+            return res.status(200, 'INSERTION EN BASE DU TRACK OK'); 
+        } catch (error) {
+            next(error)
+        }
+} );
 
 module.exports = router;
