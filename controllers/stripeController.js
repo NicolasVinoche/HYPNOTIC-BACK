13020f1e04d1e13cb9c45601710ac38b66f0c3e9
SamiFekkar <mr.fekkar.sami@gmail.com>
2021-05-03 18:52:49 +0200

    changement stripe

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -1,25 +1,27 @@
 // const cors = require("cors");
-const express = require("express");
-const stripe = require("stripe")("sk_test_51IgW8cIXwT38my0aJiBhw4YHO8xtVt49kOEV7NONO251J7TaZBhW402AUj0s7FMYdgP0ojiq4CnP5WX5q5qChrPI00yochtDIm"); 
+const express = require('express');
+const stripe = require('stripe')(
+  'sk_test_51IgW8cIXwT38my0aJiBhw4YHO8xtVt49kOEV7NONO251J7TaZBhW402AUj0s7FMYdgP0ojiq4CnP5WX5q5qChrPI00yochtDIm'
+);
 const { uuid } = require('uuidv4');
 const userDataMapper = require('../dataMappers/userDataMapper');
-var jwtUtils = require('../utils/jwt'); 
+var jwtUtils = require('../utils/jwt');
 var cors = require('cors');
 
 const app = express();
 
-var corsOptions = { 
-    origin: 'https://hypnoticpeafowl.com/',
-    allowedHeaders: 'Content-Type, Accept, Origin, Authorization',
-    preflightContinue: false,
-    methods: 'GET, PUT, POST, DELETE, PATCH',
-    credentials: true
+var corsOptions = {
+  origin: 'https://hypnoticpeafowl.com/',
+  allowedHeaders: 'Content-Type, Accept, Origin, Authorization',
+  preflightContinue: false,
+  methods: 'GET, PUT, POST, DELETE, PATCH',
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.use(express.json()); 
+app.use(express.json());
 // var allowCrossDomain = function (req, res, next) {
- //    res.header('Access-Control-Allow-Origin', ['http://localhost:8080/', 'https://sami-fekkar.xyz/']);
+//    res.header('Access-Control-Allow-Origin', ['http://localhost:8080/', 'https://sami-fekkar.xyz/']);
 //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
 //     res.header('Access-Control-Allow-Headers', 'Accept, Accept-Language, Content-Language, Upgrade-Insecure-Requests, Content-Type, X-requested-With, X-HTTP-Method-Override, X-Custom-Header, Content-Encoding');
 //     res.header('Access-Control-Allow-Credentials', true);
@@ -28,104 +30,101 @@ app.use(express.json());
 // };
 // app.use(allowCrossDomain);
 module.exports = {
+  stripeCheckout: async function (req, res, next) {
+    try {
+      const { product, email } = req.body;
+      const checkEmail = await userDataMapper.findUser(email);
 
-    stripeCheckout: async function(req, res, next) {
+      if (checkEmail) {
+        // On fait la somme de tout les produits et on la stocke
+        const allProductPrice = product
+          .map((item) => item.price)
+          .reduce((memo, val) => memo + val);
+
+        const idempotencyKey = uuid();
+        const paymentIntent = await stripe.paymentIntents.create(
+          {
+            amount: Number(allProductPrice) * 100,
+            currency: 'EUR',
+            metadata: { integration_check: 'accept_a_payment' },
+            receipt_email: email,
+          },
+          {
+            // empêche les bugs de paiement multiple
+            idempotencyKey,
+          }
+        );
+
+        res.json({ client_secret: paymentIntent['client_secret'] });
+      } else {
+        return res.status(400).json({ errors: [`Utilisateur introuvable`] });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  stripeSub: async function (req, res, next) {
+    const plan = await stripe.plans.create({
+      amount: 1500, // prix en centimes
+      currency: 'eur',
+      interval: 'month',
+      product: 'prod_JLlNaYM6K2gD5p', // ID PRODUCT DE L'ABONNEMENT PAR MOIS
+    });
 
     try {
-        const { product, email } = req.body;
-        const checkEmail = await userDataMapper.findUser(email); 
+      const { email, payment_method } = req.body;
+      const checkEmail = await userDataMapper.findUser(email);
 
-        if (checkEmail) {
+      if (checkEmail) {
+        const customer = await stripe.customers.create({
+          payment_method: payment_method,
+          email: email,
+          invoice_settings: {
+            default_payment_method: payment_method,
+          },
+        });
 
-            // On fait la somme de tout les produits et on la stocke
-            const allProductPrice = product.map(item => item.price).reduce((memo, val) => memo + val)
-        
-    
-            const idempotencyKey = uuid();
-            const paymentIntent = await stripe.paymentIntents.create(
-                {
-                    amount: allProductPrice * 100,
-                    currency: "EUR",
-                    metadata: {integration_check: 'accept_a_payment'},
-                    receipt_email: email,
-                },
-                {   // empêche les bugs de paiement multiple
-                    idempotencyKey
-                }
-            ); 
+        const subscription = await stripe.subscriptions.create({
+          customer: customer.id,
+          items: [{ price: plan.id }], // FONCTIONNEL : price_1IgtTzIXwT38my0apodcr4Yn
+          cancel_at_period_end: true, // l'abonnement s'arrête à la fin de la 1ère période
+          // expand: ['latest_invoice.payment_intent']
+        });
 
-            res.json({'client_secret': paymentIntent['client_secret']})
-        } else {
-            return res.status(400).json({errors :[`Utilisateur introuvable`]});
+        console.log(subscription);
+
+        //   const status = subscription['latest_invoice']['payment_intent']['status']
+        //   const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
+        // console.log('STATUS CHELOU', status);
+        // console.log('CLIENT CHELOU', client_secret);
+
+        if (subscription.status === 'active') {
+          //.status === 'active'
+          const subscriber = await userDataMapper.subscriber(email);
+          await userDataMapper.subscriptionEnd(
+            subscription.current_period_end,
+            customer.email
+          );
+          res.json({
+            // 'client_secret': client_secret,
+            // 'status': status,
+            role: subscriber.role,
+            userId: subscriber.id,
+            first_name: subscriber.first_name,
+            last_name: subscriber.last_name,
+            email: subscriber.email,
+            pseudo: subscriber.pseudo,
+            isadmin: subscriber.isadmin,
+            token: jwtUtils.generateTokenForUser(subscriber),
+            current_period_end: subscription.current_period_end,
+          });
         }
-
-    }   catch (error){
-            next(error);
-        }  
-    },
-
-    stripeSub: async function(req, res, next) {  
-
-        const plan = await stripe.plans.create({
-            amount: 1500, // prix en centimes
-            currency: 'eur',
-            interval: 'month',
-            product: 'prod_JLlNaYM6K2gD5p',  // ID PRODUCT DE L'ABONNEMENT PAR MOIS
-            });
-        
-        try {
-            const { email, payment_method } = req.body; 
-            const checkEmail = await userDataMapper.findUser(email); 
-
-            if (checkEmail) {
-
-                    const customer = await stripe.customers.create({
-                        payment_method: payment_method,
-                        email: email,
-                        invoice_settings: {
-                            default_payment_method: payment_method,
-                        },
-                    });
-                    
-                    const subscription = await stripe.subscriptions.create({
-                        customer: customer.id,
-                        items: [{price: plan.id}], // FONCTIONNEL : price_1IgtTzIXwT38my0apodcr4Yn
-                        cancel_at_period_end: true // l'abonnement s'arrête à la fin de la 1ère période
-                       // expand: ['latest_invoice.payment_intent']
-                    }); 
-         
-                console.log(subscription)
-                
-                //   const status = subscription['latest_invoice']['payment_intent']['status']
-                //   const client_secret = subscription['latest_invoice']['payment_intent']['client_secret']
-                // console.log('STATUS CHELOU', status);
-                // console.log('CLIENT CHELOU', client_secret);
-
-                if(subscription.status === 'active') { //.status === 'active'
-                    const subscriber = await userDataMapper.subscriber(email);
-                    await userDataMapper.subscriptionEnd(subscription.current_period_end, customer.email);
-                    res.json({
-                        // 'client_secret': client_secret, 
-                        // 'status': status,
-                        'role': subscriber.role,
-                        'userId': subscriber.id,
-                        'first_name': subscriber.first_name,
-                        'last_name': subscriber.last_name,
-                        'email': subscriber.email,
-                        'pseudo': subscriber.pseudo,
-                        'isadmin': subscriber.isadmin,
-                        'token': jwtUtils.generateTokenForUser(subscriber),
-                        'current_period_end': subscription.current_period_end
-                });
-                }
-            } else {
-                
-                return res.status(400).json({errors :[`Utilisateur introuvable`]});
-            }
-        } catch(error) {
-            next(error);
-        } 
-    
-    }, 
-}
-    
+      } else {
+        return res.status(400).json({ errors: [`Utilisateur introuvable`] });
+      }
+    } catch (error) {
+      next(error);
+    }
+  },
+};
